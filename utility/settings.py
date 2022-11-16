15417fdca7059250f3499565ae248a978676be98
Karthick Sakkaravarthi <37908485+karthicksakkaravarti@users.noreply.github.com>
2022-11-16 12:22:17 +0530

    Initial

--- utility/settings.py
+++ utility/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9bx#t$*%bdfb2plr2zq_2tmd)&(!=bnroes#!)xrxtexvutxin
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
